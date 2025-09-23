9fab53e21da8c93c51088e28c1934b9ad56ae3dc
AnaPaulaMicheletti <108703298+AnaPaulaMicheletti@users.noreply.github.com>
2025-09-23 16:50:05 -0300

    soma.cpp
    O programa calcula os valores atribuídos ao X e Y.

---
+++ soma.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //entrada
+    int x, y, soma;
+
+    cout << "Digite o valor de X: ";
+    cin >> x;
+
+    cout << "Digite o valor de Y: ";
+    cin >> y;
+
+    //processamento
+    soma = x + y;
+
+    //saida
+    cout << "Resultado: " << soma;
+
+    return 0;
+}
