67abbaae67b629202b0b0df26f8b6c3e5d39a5b0
AnaPaulaMicheletti <108703298+AnaPaulaMicheletti@users.noreply.github.com>
2025-11-15 00:56:01 -0300

    Add files via upload

---
+++ 4-vetores/Exemplo_2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    int vetor[4]; 
+
+
+    c = (a + b) / 2;
+    c = c - 40;
+    vetor[4] = a + b + c; //o numero 4 indica q o vetor vai ter 4 elementos (numeros, palavras...)
+                          //começa a conatr a partir do 0(zero). Ou seja os valores seram 0, 1, 2, 3.
+
+    return 0;
+}
+
+
+
+
+//TERMINAR
