50e0a0f4820b9f3618feab923e60b03be1026af1
AnaPaulaMicheletti <108703298+AnaPaulaMicheletti@users.noreply.github.com>
2025-11-25 21:02:24 -0300

    Add files via upload

---
+++ for/soma_impares.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+
+    cout << "Digite dois numeros: ";
+    cin >> x;
+    cin >> y;
+
+    if(x, y <= 1){
+
+    }else{
+
+    }
+
+    for(int i = 1; i <){
+
+    }
+
+    return 0;
+}
